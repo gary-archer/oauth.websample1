f070a836d94f5299bb577c2194847d3ba99eac2c
Gary Archer <Gary.Archer@collinsongroup.com>
2020-01-02 17:36:37 +0000

    Updates

--- spa/src/views/headerButtonsView.ts
+++ spa/src/views/headerButtonsView.ts
@@ -28,10 +28,10 @@ export class HeaderButtonsView {
                 <button id='btnHome' type='button' class='btn btn-primary btn-block p-1'>Home</button>
             </div>
             <div class='col-4 my-2 d-flex'>
-                <button id='btnExpireAccessToken' type='button' class='btn btn-primary btn-block p-1 sessionbutton'>Expire Token</button>
+                <button id='btnExpireAccessToken' type='button' disabled class='btn btn-primary btn-block p-1 sessionbutton'>Expire Token</button>
             </div>
             <div class='col-4 my-2 d-flex'>
-                <button id='btnRefreshData' type='button' class='btn btn-primary btn-block p-1 sessionbutton'>Refresh Data</button>
+                <button id='btnRefreshData' type='button' disabled class='btn btn-primary btn-block p-1 sessionbutton'>Refresh Data</button>
             </div>
         </div>`;
         $('#headerbuttons').html(html);
