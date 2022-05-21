456f58c9385f930de0cdb981b0f24cea297eb4ff
gary-archer <garyarcher36@gmail.com>
2022-05-21 08:43:12 +0100

    Updates

--- api/start.sh
+++ api/start.sh
@@ -7,15 +7,15 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 npm install
 
+#
+# On Linux ensure that you have first granted Node.js permissions to listen on port 80:
+# - sudo setcap 'cap_net_bind_service=+ep' $(which node)
+#
+npm start
+
+#
+# Prevent automatic terminal closure on Linux
+#
 if [ "$(uname -s)" == 'Linux' ]; then
-
-    # On Linux we must run the underlying command as root, in order to listen on ports below 1024
-    sudo ./node_modules/.bin/ts-node --files ./src/host/startup/app.ts
-    read -n 1
-
-else
-
-    # On Windows or macOS we just run the start command
-    npm start
+  read -n 1
 fi
-
--- spa/start.sh
+++ spa/start.sh
@@ -8,6 +8,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 npm install
 npm start
 
+#
+# Prevent automatic terminal closure on Linux
+#
 if [ "$(uname -s)" == 'Linux' ]; then
   read -n 1
 fi
