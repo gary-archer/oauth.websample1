ccf58dbd33b809f236acfaaf57d039cbb4b52559
gary <garyarcher36@gmail.com>
2025-12-24 08:01:11 +0000

    Updates

--- spa/vite.config.ts
+++ spa/vite.config.ts
@@ -1,21 +1,33 @@
-import {defineConfig} from 'vite';
+import {ConfigEnv, defineConfig, UserConfig} from 'vite';
 
-export default defineConfig({
-    build: {
-        sourcemap: true,
-        //minify: true,
-        outDir: './dist',
-        rollupOptions: {
-            input: './src/app/app.ts',
-            output: {
-                entryFileNames: '[name].bundle.js',
-                chunkFileNames: '[name].bundle.js',
-                manualChunks: (id: string) => {
-                    if (id.includes('node_modules')) {
-                        return 'vendor';
-                    }
+export default defineConfig((configEnv: ConfigEnv) => {
+
+    const userConfig: UserConfig = {
+
+        build: {
+            minify: configEnv.mode === 'production',
+            outDir: 'dist',
+            sourcemap: true,
+
+            rollupOptions: {
+                input: './src/app/app.ts',
+                output: {
+
+                    entryFileNames: '[name].bundle.js',
+                    chunkFileNames: '[name].bundle.js',
+
+                    manualChunks: (id: string) => {
+                        if (id.includes('node_modules')) {
+                            return 'vendor';
+                        }
+                    },
                 },
-            },
-        }
-    }
+            }
+        },
+        define: {
+            IS_DEBUG: `${configEnv.mode === 'development'}`
+        },
+    };
+
+    return userConfig;
 });
